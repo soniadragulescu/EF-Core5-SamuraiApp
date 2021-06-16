848956eeae24daf72fcdc2982eb9e284024afb9f
soniadragulescu <soniadr1999@yahoo.com>
2021-06-16 20:43:08 +0300

    create db

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -7,5 +7,12 @@ namespace SamuraiApp.Data
 	{
 		public DbSet<Samurai> Samurais { get; set; }
 		public DbSet<Quote> Quotes { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(
+				"Data Source= (localdb)\\MSSQLLocalDb; Initial Catalog=SamuraiAppData");
+			//base.OnConfiguring(optionsBuilder);
+		}
 	}
 }
