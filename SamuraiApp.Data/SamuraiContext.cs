6382c63a6075f8897633378c20919b3e8b2c0a8c
soniadragulescu <soniadr1999@yahoo.com>
2021-06-16 20:03:02 +0300

    added db context

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -1,6 +1,11 @@
-﻿namespace SamuraiApp.Data
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.Data
 {
-	public class SamuraiContext
+	public class SamuraiContext: DbContext
 	{
+		public DbSet<Samurai> Samurais { get; set; }
+		public DbSet<Quote> Quotes { get; set; }
 	}
 }
