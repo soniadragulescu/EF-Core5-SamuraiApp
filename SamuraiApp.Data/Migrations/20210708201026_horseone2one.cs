07a57772898e5bac15aab63aaf0fc998ba1823db
soniadragulescu <soniadr1999@yahoo.com>
2021-07-08 23:13:25 +0300

    added one-to-one relationship

---
+++ SamuraiApp.Data/Migrations/20210708201026_horseone2one.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SamuraiApp.Data.Migrations
+{
+    public partial class horseone2one : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BattleSamurai_Battle_BattleId",
+                table: "BattleSamurai");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Battle",
+                table: "Battle");
+
+            migrationBuilder.RenameTable(
+                name: "Battle",
+                newName: "Battles");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Battles",
+                table: "Battles",
+                column: "BattleId");
+
+            migrationBuilder.CreateTable(
+                name: "Horse",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SamuraiId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Horse", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Horse_Samurais_SamuraiId",
+                        column: x => x.SamuraiId,
+                        principalTable: "Samurais",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Horse_SamuraiId",
+                table: "Horse",
+                column: "SamuraiId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BattleSamurai_Battles_BattleId",
+                table: "BattleSamurai",
+                column: "BattleId",
+                principalTable: "Battles",
+                principalColumn: "BattleId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BattleSamurai_Battles_BattleId",
+                table: "BattleSamurai");
+
+            migrationBuilder.DropTable(
+                name: "Horse");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Battles",
+                table: "Battles");
+
+            migrationBuilder.RenameTable(
+                name: "Battles",
+                newName: "Battle");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Battle",
+                table: "Battle",
+                column: "BattleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BattleSamurai_Battle_BattleId",
+                table: "BattleSamurai",
+                column: "BattleId",
+                principalTable: "Battle",
+                principalColumn: "BattleId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -7,6 +7,7 @@ namespace SamuraiApp.Data
 	{
 		public DbSet<Samurai> Samurais { get; set; }
 		public DbSet<Quote> Quotes { get; set; }
+		public DbSet<Battle> Battles { get; set; }
 
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
