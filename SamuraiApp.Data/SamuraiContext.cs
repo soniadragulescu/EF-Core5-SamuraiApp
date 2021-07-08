17c849bd76c12e2f8a54c3a4b6573777815aec4f
soniadragulescu <soniadr1999@yahoo.com>
2021-07-08 23:07:24 +0300

    added many-to-many relationship

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -12,7 +12,19 @@ namespace SamuraiApp.Data
 		{
 			optionsBuilder.UseSqlServer(
 				"Data Source= (localdb)\\MSSQLLocalDb; Initial Catalog=SamuraiAppData");
-			//base.OnConfiguring(optionsBuilder);
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Samurai>()
+			 .HasMany(s => s.Battles)
+			 .WithMany(b => b.Samurais)
+			 .UsingEntity<BattleSamurai>
+			  (bs => bs.HasOne<Battle>().WithMany(),
+			   bs => bs.HasOne<Samurai>().WithMany())
+			 .Property(bs => bs.DateJoined)
+			 .HasDefaultValueSql("getdate()");
+
 		}
 	}
 }
--- SamuraiApp.Domain/Samurai.cs
+++ SamuraiApp.Domain/Samurai.cs
@@ -7,5 +7,7 @@ namespace SamuraiApp.Domain
 		public int Id { get; set; }
 		public string Name { get; set; }
 		public List<Quote> Quotes { get; set; } = new List<Quote>();
+		public List<Battle> Battles { get; set; } = new List<Battle>();
+		//public Horse Horse { get; set; }
 	}
 }
